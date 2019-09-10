d7fdabafd95ec4841d84125b6785e17c097cb74c
Julian Seibert <julianseibert@gmx.de>
2019-09-10 19:02:14 +0200

    refactor

--- default-units/zone.js
+++ default-units/zone.js
@@ -1,219 +1,269 @@
 module.exports = {
-    metadata: {
-        plugin: "zone",
-        label: "Zone",
-        role: "actor",
-        family: "thermostat",
-        deviceTypes: ["tado/tadoConnector"],
-        services: [
-            //TODO
-            //identifyZoneDevices
-        ],
-        events: [],
-        state: [{
-            id: 'zoneName',
-            label: 'Zone Name',
-            type: {
-                id: 'string',
-            },
-        }, {
-            id: 'runningMode',
-            label: 'Running Mode',
-            type: {
-                id: 'string',
-            },
-        }, {
-            id: 'temperature',
-            label: 'Temperature',
-            type: {
-                id: 'number',
-            },
-        }, {
-            id: 'setpoint',
-            label: 'Setpoint',
-            type: {
-                id: 'number',
-            },
-            unit: '°C'
-        }, {
-            id: 'humidity',
-            label: 'Humidity',
-            type: {
-                id: 'number',
-            },
-            unit: '%'
-        }, {
-            id: 'heatingPower',
-            label: 'Heating Power',
-            type: {
-                id: 'number',
-            },
-            unit: '%'
-        }, {
-            id: 'openWindowDetected',
-            label: 'Open Window Detected',
-            type: {
-                id: 'boolean',
-            },
-        }, {
-            id: 'openWindowDetectTimestamp',
-            label: 'Open Window detect Timestamp',
-            type: {
-                id: 'string',
-            },
-        }, {
-            id: 'power',
-            label: 'Power',
-            type: {
-                id: 'string',
-            },
-        }, {
-            id: 'linkState',
-            label: 'Link State',
-            type: {
-                id: 'string',
-            },
-        },],
-        configuration: [{
-            id: 'zoneId',
-            label: 'Zone ID',
-            type: {
-                id: 'integer'
-            },
-            defaultValue: '',
-        }, {
-            id: 'pollingIntervalTime',
-            label: 'Polling Interval Time',
-            type: {
-                id: 'integer'
-            },
-            unit: 's',
-            defaultValue: 10
-        }]
-    },
-    create: function () {
-        return new Zone();
-    }
+  metadata: {
+    plugin: 'zone',
+    label: 'Zone',
+    role: 'actor',
+    family: 'thermostat',
+    deviceTypes: ['tado/tadoConnector'],
+    services: [
+    ],
+    events: [],
+    state: [{
+      id: 'zoneName',
+      label: 'Zone Name',
+      type: {
+        id: 'string',
+      },
+    }, {
+      id: 'runningMode',
+      label: 'Running Mode',
+      type: {
+        id: 'string',
+      },
+    }, {
+      id: 'temperature',
+      label: 'Temperature',
+      type: {
+        id: 'number',
+      },
+    }, {
+      id: 'setpoint',
+      label: 'Setpoint',
+      type: {
+        id: 'number',
+      },
+      unit: '°C',
+    }, {
+      id: 'humidity',
+      label: 'Humidity',
+      type: {
+        id: 'number',
+      },
+      unit: '%',
+    }, {
+      id: 'heatingPower',
+      label: 'Heating Power',
+      type: {
+        id: 'number',
+      },
+      unit: '%',
+    }, {
+      id: 'openWindowDetected',
+      label: 'Open Window Detected',
+      type: {
+        id: 'boolean',
+      },
+    }, {
+      id: 'openWindowDetectTimestamp',
+      label: 'Open Window detect Timestamp',
+      type: {
+        id: 'string',
+      },
+    }, {
+      id: 'power',
+      label: 'Power',
+      type: {
+        id: 'string',
+      },
+    }, {
+      id: 'linkState',
+      label: 'Link State',
+      type: {
+        id: 'string',
+      },
+    }],
+    configuration: [{
+      id: 'zoneId',
+      label: 'Zone ID',
+      type: {
+        id: 'integer',
+      },
+      unit: 'id',
+      defaultValue: '',
+    }, {
+      id: 'showTemperature',
+      label: 'Show temperature',
+      type: {
+        id: 'boolean',
+      },
+      defaultValue: true,
+    }, {
+      id: 'minSetpoint',
+      label: 'Min Setpoint',
+      type: {
+        id: 'integer',
+      },
+      unit: 'grad',
+      defaultValue: 5,
+    }, {
+      id: 'maxSetpoint',
+      label: 'Max setpoint',
+      type: {
+        id: 'integer',
+      },
+      unit: 'grad',
+      defaultValue: 25,
+    }, {
+      id: 'pollingIntervalTime',
+      label: 'Polling Interval Time',
+      type: {
+        id: 'integer',
+      },
+      unit: 's',
+      defaultValue: 10,
+    }],
+  },
+  create: function() {
+    return new Zone();
+  },
 };
 
-var q = require('q');
+const q = require('q');
+const _ = require('lodash');
+const Helper = require('../lib/helper');
 
 /**
  *
  */
 function Zone() {
-    /**
-     *
-     */
-    Zone.prototype.start = function () {
-        let deferred = q.defer();
-        this.state = {};
-        if (this.isSimulated()) {
-
-            //TODO
-            deferred.resolve();
-        } else {
-            this.updateInterval = setInterval(() => {
-                this.device.tado.getZoneState(this.device.configuration.homeId, this.configuration.zoneId)
-                    .then((res) => {
-                        if (res) {
-                            this.state.runningMode = res.tadoMode;
-                            this.state.temperature = res.sensorDataPoints.insideTemperature.celsius;
-                            this.state.heatingPower = res.activityDataPoints.heatingPower.percentage;
-                            this.state.power = res.setting.power;
-                            this.state.humidity = res.sensorDataPoints.humidity.percentage;
-                            this.state.linkState = res.link.state;
-
-                            if (res.setting.temperature) {
-                                this.state.setpoint = res.setting.temperature.celsius;
-                            } else {
-                                this.state.setpoint = 'n/a';
-                            }
-
-                            if (!this.state.zoneName) {
-                                this.state.zoneName = this.device.state.zones.find((zone) => {
-                                    return zone.id === this.configuration.zoneId;
-                                }).name;
-                            }
-
-                            if (res.openWindow) {
-                                if (!this.state.openWindowDetected) {
-                                    this.publishEvent("openWindowDetected");
-                                }
-                                this.state.openWindowDetected = true;
-                                this.state.openWindowDetectTimestamp = res.openWindow.detectedTime;
-                            } else {
-                                this.state.openWindowDetected = false;
-                                this.state.openWindowDetectTimestamp = '';
-                            }
-                            this.publishStateChange();
-                        }
-                    })
-                    .catch((err) => {
-                        this.logError("Error while updating Zone: ", err)
-                    });
-            }, this.configuration.pollingIntervalTime * 1000);
+  /**
+   *
+   */
+  Zone.prototype.start = function() {
+    let deferred = q.defer();
+    this.state = {};
+    if (this.isSimulated()) {
 
 
-            deferred.resolve();
-        }
+      deferred.resolve();
+    } else {
+      this.device.tado.initialized
+        .then(() => {
+          this.updateInterval = setInterval(() => {
+            this.device.tado.getZoneState(this.configuration.zoneId)
+              .then((res) => {
+                if (res) {
+                  let oldState = _.clone(this.state);
 
-        return deferred.promise;
-    };
+                  this.state.runningMode = res.tadoMode;
+                  this.state.temperature = res.sensorDataPoints.insideTemperature.celsius;
+                  this.state.heatingPower = res.activityDataPoints.heatingPower.percentage;
+                  this.state.power = res.setting.power;
+                  this.state.humidity = res.sensorDataPoints.humidity.percentage;
 
 
-    /**
-     *
-     */
-    Zone.prototype.getState = function () {
-        //this.update();
-        return this.state;
-    };
+                  this.state.linkState = res.link.state;
+
+                  let oldOpState = _.clone(this.operationalState);
+
+                  if (this.state.linkState !== 'ONLINE') {
+                    this.operationalState.status = 'ERROR';
+                    this.operationalState.message = 'Thermostat offline';
+                  } else {
+                    this.operationalState.status = 'OK';
+                    this.operationalState.message = 'Thermostat online';
+                  }
+
+                  let newOpState = Helper.diff(this.operationalState, oldOpState);
+                  if (Object.keys(newOpState).length > 0 && newOpState.constructor === Object) {
+                    this.publishOperationalStateChange(newOpState);
+                  }
 
 
-    // /**
-    //  * TODO
-    //  */
-    // Zone.prototype.identifyZoneDevices = function () {
-    //     for (let deviceId of this.state.deviceId) {
-    //         this.device.tado.identifyDevice(deviceId)
-    //             .then((res) => {
-    //                 this.logDebug("Succseful identified device: ", deviceId);
-    //             })
-    //             .catch((err) => {
-    //                 this.logError("Device indetification unsucessful for device: ", deviceId, " with error: ", err);
-    //             });
-    //     }
-    // };
+                  if (res.setting.temperature) {
+                    this.state.setpoint = res.setting.temperature.celsius;
+                  } else {
+                    this.state.setpoint = 'n/a';
+                  }
 
-    /**
-     *
-     */
-    Zone.prototype.setState = function (state) {
-        if ((state)) {
-            if (state.setpoint !== this.state.setpoint) {
-                this.device.tado.setZoneOverlay(this.device.configuration.homeId, this.configuration.zoneId, 'on', state.setpoint, 'auto')
-                    .then((resp) => {
-                        this.logInfo("Succseful adjusted setpoint to: ", state.setpoint);
-                        this.state = state;
-                    })
-                    .catch((err) => {
-                        this.logDebug("Cannot adjusted setpoint!");
-                    });
-            }
-        } else {
-            this.logError("Cannot setState without state");
-        }
-    };
+                  if (!this.state.zoneName) {
+                    this.state.zoneName = this.device.state.zones.find((zone) => {
+                      return zone.id === this.configuration.zoneId;
+                    }).name;
+                  }
 
-    /**
-     *
-     */
-    Zone.prototype.stop = function () {
-        if (this.isSimulated()) {
+                  if (res.openWindow) {
+                    if (!this.state.openWindowDetected) {
+                      this.publishEvent('openWindowDetected');
+                    }
+                    this.state.openWindowDetected = true;
+                    this.state.openWindowDetectTimestamp = res.openWindow.detectedTime;
+                  } else {
+                    this.state.openWindowDetected = false;
+                    this.state.openWindowDetectTimestamp = '';
+                  }
 
-        } else {
-            clearInterval(this.updateInterval);
-        }
+                  let newState = Helper.diff(this.state, oldState);
+                  if (Object.keys(newState).length > 0 && newState.constructor === Object) {
+                    this.publishStateChange(newState);
+                  }
+                }
+              })
+              .catch((err) => {
+                this.logError('Error while updating Zone: ', err);
+              });
+          }, this.configuration.pollingIntervalTime * 1000);
+        });
+
+      deferred.resolve();
     }
+
+    return deferred.promise;
+  };
+
+
+  /**
+   *
+   */
+  Zone.prototype.getState = function() {
+    //this.update();
+    return this.state;
+  };
+
+
+  // /**
+  //  *
+  //  */
+  // Zone.prototype.identifyZoneDevices = function () {
+  //     for (let deviceId of this.state.deviceId) {
+  //         this.device.tado.identifyDevice(deviceId)
+  //             .then((res) => {
+  //                 this.logDebug("Succseful identified device: ", deviceId);
+  //             })
+  //             .catch((err) => {
+  //                 this.logError("Device indetification unsucessful for device: ", deviceId, " with error: ", err);
+  //             });
+  //     }
+  // };
+
+  /**
+   * //TODO Handle fast calls
+   */
+  Zone.prototype.setState = function(state) {
+    if ((state)) {
+      if (state.setpoint !== this.state.setpoint) {
+        this.device.tado.setZoneOverlay(this.configuration.zoneId, state.setpoint)
+          .then(() => {
+            this.logInfo('Succseful adjusted setpoint to: ', state.setpoint);
+            this.state = state;
+          })
+          .catch((err) => {
+            this.logDebug('Cannot adjusted setpoint!', err);
+          });
+      }
+    } else {
+      this.logError('Cannot setState without state');
+    }
+  };
+
+  /**
+   *
+   */
+  Zone.prototype.stop = function() {
+    if (this.isSimulated()) {
+
+    } else {
+      clearInterval(this.updateInterval);
+    }
+  };
 }
--- tadoConnector.js
+++ tadoConnector.js
@@ -1,165 +1,141 @@
 module.exports = {
-    metadata: {
-        plugin: 'tadoConnector',
-        label: 'Tado Connector',
-        family: 'controller',
-        actorTypes: [],
-        sensorTypes: [],
-        events: [],
-        services: [
-            //TODO ADD SERVICES
-        ],
-        state: [{
-            id: 'homes', label: 'Homes',
-            type: {
-                id: 'object',
-            },
-        }, {
-            id: 'zones', label: 'Zones',
-            type: {
-                id: 'object',
-            },
-        }, {
-            id: 'weatherState', label: 'Weather state',
-            type: {
-                id: 'string',
-            },
-        }, {
-            id: 'outsideTemperature', label: 'Outside temperature',
-            type: {
-                id: 'number',
-            },
-            unit: '°C'
-        }, {
-            id: 'solarIntensity', label: 'Solar intensity',
-            type: {
-                id: 'number',
-            },
-            unit: '%'
-        }],
-        configuration: [{
-            label: 'Username (Email)',
-            id: 'username',
-            type: {
-                id: 'string',
-            },
-            defaultValue: '',
-        }, {
-            label: 'Password',
-            id: 'password',
-            type: {
-                id: 'password',
-            },
-            defaultValue: '',
-        }, {
-            label: 'Home ID',
-            id: 'homeId',
-            type: {
-                id: 'string',
-            },
-            defaultValue: '',
-        }, {
-            label: 'Weather update interval',
-            id: 'weatherUpdateInterval',
-            type: {
-                id: 'integer',
-            },
-            defaultValue: 10,
-            unit: "s"
-        }],
-    },
+  metadata: {
+    plugin: 'tadoConnector',
+    label: 'Tado Connector',
+    family: 'controller',
+    actorTypes: [],
+    sensorTypes: [],
+    events: [],
+    services: [
+      //TODO ADD SERVICES
+    ],
+    state: [{
+      id: 'homes', label: 'Homes',
+      type: {
+        id: 'object',
+      },
+    }, {
+      id: 'zones', label: 'Zones',
+      type: {
+        id: 'object',
+      },
+    }, {
+      id: 'weatherState', label: 'Weather state',
+      type: {
+        id: 'string',
+      },
+    }, {
+      id: 'outsideTemperature', label: 'Outside temperature',
+      type: {
+        id: 'number',
+      },
+      unit: '°C',
+    }, {
+      id: 'solarIntensity', label: 'Solar intensity',
+      type: {
+        id: 'number',
+      },
+      unit: '%',
+    }],
+    configuration: [{
+      label: 'Username (Email)',
+      id: 'username',
+      type: {
+        id: 'string',
+      },
+      defaultValue: '',
+    }, {
+      label: 'Password',
+      id: 'password',
+      type: {
+        id: 'password',
+      },
+      defaultValue: '',
+    }, {
+      label: 'Home ID',
+      id: 'homeId',
+      type: {
+        id: 'string',
+      },
+      defaultValue: '',
+    }, {
+      label: 'Weather update interval',
+      id: 'weatherUpdateInterval',
+      type: {
+        id: 'integer',
+      },
+      defaultValue: 10,
+      unit: 's',
+    }],
+  },
 
-    create: function () {
-        return new TadoConnector();
+  create: function() {
+    return new TadoConnector();
 
-    },
+  },
 };
 
 
 function TadoConnector() {
-    this.state = {};
+  this.state = {};
 }
 
+const q = require('q');
+const deferred = q.defer();
+const TadoAdapter = require('./lib/tadoAdapter');
+const Helper = require('./lib/helper');
+const _ = require('lodash');
 /**
  *
  *
  */
-TadoConnector.prototype.start = function () {
-    let q = require('q');
-    let deferred = q.defer();
-    const Tado = require('node-tado-client');
+TadoConnector.prototype.start = function() {
 
-    if (this.isSimulated()) {
-        this.logDebug("Starting Tado Connector in simulated mode.");
+  if (this.isSimulated()) {
+    this.logDebug('Starting Tado Connector in simulated mode.');
 
-        deferred.resolve();
-    } else {
+    deferred.resolve();
+  } else {
 
-        this.tado = new Tado();
-
-        this.tado.login(this.configuration.username, this.configuration.password)
-            .then(() => {
-                this.tado.getMe()
-                    .then(resp => {
-                        this.state.homes = resp.homes;
-
-                        if (!this.configuration.homeId) {
-                            this.configuration.homeId = this.state.homes[0].id;
-                            this.logDebug("No HomeID configured. Set to the first found: ", this.configuration.homeId);
-                        }
-
-                        this.tado.getZones(this.configuration.homeId)
-                            .then(resp => {
-                                this.state.zones = [];
-                                for (let zone of resp) {
-                                    this.state.zones.push({
-                                        id: zone.id,
-                                        name: zone.name
-                                    });
-                                }
-                                this.publishStateChange();
-                            })
-                            .catch((err) => {
-                                this.logError("Unable to get Zones with error: ", err);
-                            });
-
-                        this.weatherInterval = setInterval(() => {
-                            this.tado.getWeather(this.configuration.homeId)
-                                .then((res) => {
-                                    this.state.solarIntensity = res.solarIntensity.percentage;
-                                    this.state.outsideTemperature = res.outsideTemperature.celsius;
-                                    this.state.weatherState = res.weatherState.value;
-                                    this.publishStateChange();
-                                })
-                                .catch((err) => {
-                                    this.logDebug("Unable to update weather state with error: ", err);
-                                });
-                        }, this.configuration.weatherUpdateInterval * 1000);
-
-
-                    })
-                    .catch(err => {
-                        this.logError("Unable to connect to User: ", this.configuration.username, " with error: ", err);
-                    });
-
-
-            })
-            .catch(err => {
-                this.operationalState.status = "ERROR";
-                this.operationalState.message = err;
-                this.publishOperationalStateChange();
-            });
-
-        this.operationalState.status = "PENDING";
-        this.operationalState.message = "INITIALIZING...";
+    this.tado = new TadoAdapter();
+    this.tado.initialized = this.tado.initialize(this.configuration.username, this.configuration.password, this.configuration.homeId)
+      .then((homeId) => {
+        this.operationalState.status = 'OK';
+        this.operationalState.message = 'Successful connected to Home: ' + homeId;
         this.publishOperationalStateChange();
 
-        deferred.resolve();
-    }
+        this.weatherInterval = setInterval(() => {
+          this.tado.getWeather()
+            .then((weather) => {
+              let oldState = _.clone(this.state);
+              this.state.solarIntensity = weather.solarIntensity.percentage;
+              this.state.outsideTemperature = weather.outsideTemperature.celsius;
+              this.state.weatherState = weather.weatherState.value;
 
-    return deferred.promise;
+              let newState = Helper.diff(this.state, oldState);
+              if (Object.keys(newState).length > 0 && newState.constructor === Object) {
+                this.publishStateChange(newState);
+              }
+            })
+            .catch((err) => {
+              this.logDebug('Unable to update weather state with error: ', err);
+            });
+        }, this.configuration.weatherUpdateInterval * 1000);
+      })
+      .then(() => {
+        this.tado.getZones()
+          .then(zones => {
+            this.state.zones = zones;
+            this.publishStateChange();
+          })
+          .catch((err) => {
+            this.logError('Unable to get Zones with error: ', err);
+          });
+      });
+  }
 
-
+  deferred.resolve();
+  return deferred.promise;
 };
 
 
@@ -167,25 +143,23 @@ TadoConnector.prototype.start = function () {
  * stop - TadoConnector.
  *
  */
-TadoConnector.prototype.stop = function () {
-    //TODO;
+TadoConnector.prototype.stop = function() {
+  clearInterval(this.weatherInterval);
 };
 
 
 /**
  *
  *
- *
  */
-TadoConnector.prototype.setState = function (state) {
-    //TODO
+TadoConnector.prototype.setState = function(state) {
+  //TODO
 };
 
 /**
  *
  *
- *
  */
-TadoConnector.prototype.getState = function () {
-    return this.state;
+TadoConnector.prototype.getState = function() {
+  return this.state;
 };
